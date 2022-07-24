625cede80171f17887be2becc0d4769698a955d5
Jeremiah-Quaynor <jeremiah.quaynor@stu.ucc.edu.gh>
2022-07-24 01:01:14 +0000

    connecting backend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { Route, Routes } from "react-router-dom"
+import { AuthContextProvider } from "./components/AuthContext";
 import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Account from "./pages/Account";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <>
-      <Navbar />
+    <AuthContextProvider>
+    <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/account" element={<ProtectedRoute><Account /></ProtectedRoute>} />
       </Routes>
+    </AuthContextProvider>
+
     </>
   );
 }
